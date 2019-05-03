126686600557fd80763bc4f387ba5b117b527bf4
victorN <victornguyen082@gmail.com>
2019-05-03 12:47:38 -0400

    filter through tab works, but doesn't show unless clicked a tab

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,3 +1,30 @@
+import React from 'react';
+import TopBar from './components/TopBar';
+import Header from './components/Header';
+import Content from './components/Content/Content';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardData: [],
+      tabData: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default App;
+/*
 import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
@@ -13,4 +40,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App; */
